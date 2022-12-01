d250550c081765468e7559d1620f9ac717638328
kristijan-kresic-hvar <kristijan.kresic.dev@gmail.com>
2022-12-01 23:38:04 +0100

    Initial modal component setup, added fade animation to tailwind config, setup app state for modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,9 +1,52 @@
 import React from 'react'
 
-const Modal = () => {
+// third party libraries
+import PropTypes from 'prop-types'
+
+const Modal = ({ children, title, isOpen, onClose }) => {
+
+    const handleModalClick = (e) => {
+        if (e.target.getAttribute('data-type') === 'modal-overlay') {
+            onClose()
+        }
+    }
+
+    if (!isOpen) return null
     return (
-        <div>Modal</div>
+        <div
+            className="flex justify-center items-center fixed top-0 bottom-0 right-0 left-0 bg-black bg-opacity-60"
+            data-type='modal-overlay'
+            onClick={handleModalClick}
+        >
+            <div
+                className="bg-white w-4/6 h-4/6 relative animate-fade"
+            >
+                <h2
+                    className="mt-3 text-center text-xl"
+                >
+                    {title}
+                </h2>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none" viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="absolute top-3 right-3 w-8 h-8 cursor-pointer ml-auto"
+                    onClick={onClose}
+                >
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                </svg>
+                {children}
+            </div>
+        </div>
     )
+}
+
+Modal.propTypes = {
+    children: PropTypes.node.isRequired,
+    title: PropTypes.string.isRequired,
+    isOpen: PropTypes.bool.isRequired,
+    onClose: PropTypes.func.isRequired
 }
 
 export default Modal
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,19 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        fade: 'fadeIn 200ms ease-in-out forwards',
+      },
+
+      // that is actual animation
+      keyframes: theme => ({
+        fadeIn: {
+          '0%': { backgroundColor: theme('colors.transparent') },
+          '100%': { backgroundColor: theme('colors.white') },
+        },
+      }),
+    },
   },
   plugins: [],
 }
