2b6a365ccb20fd0457213c8329334c7854ac0e66
kristijan-kresic-hvar <kristijan.kresic.dev@gmail.com>
2022-12-03 23:59:40 +0100

    Initial totalCalculator component setup

--- src/components/TotalCalculator/TotalCalculator.jsx
+++ src/components/TotalCalculator/TotalCalculator.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 
-const TotalCalculator = () => {
+// third party libraries
+import PropTypes from 'prop-types'
+
+const TotalCalculator = ({ items, discountAmount }) => {
+
+    const calculateTotal = () => {
+        let total = 0
+        total = items.reduce((acc, item) => {
+            return acc + item.price
+        }, 0)
+
+        if (discountAmount) {
+            return total - (total * (discountAmount / 100))
+        }
+
+        return total
+    }
+
     return (
-        <div>TotalCalculator</div>
+        <div className="text-2xl uppercase">
+            Ukupno: <span className='font-bold'>{calculateTotal().toFixed(2).replace('.', ',')}</span>
+        </div>
     )
+}
+
+TotalCalculator.propTypes = {
+    items: PropTypes.array.isRequired,
+    discountAmount: PropTypes.number
 }
 
 export default TotalCalculator
