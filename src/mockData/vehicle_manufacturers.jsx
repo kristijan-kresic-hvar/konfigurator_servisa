665e541d2fb03ec0731f7e74ad41f95aa2535222
kristijan-kresic-hvar <kristijan.kresic.dev@gmail.com>
2022-12-05 03:07:04 +0100

    Setup useMockData hook and mockData items

---
+++ src/mockData/vehicle_manufacturers.jsx
@@ -0,0 +1,59 @@
+const vehicle_manufacturers = [
+    {
+        id: 1,
+        value: "peugeot",
+        label: "Peugeot"
+    },
+    {
+        id: 2,
+        value: "volkswagen",
+        label: "Volkswagen"
+    },
+    {
+        id: 3,
+        value: "citroen",
+        label: "Citroen"
+    },
+    {
+        id: 4,
+        value: "audi",
+        label: "Audi"
+    },
+    {
+        id: 5,
+        value: "bmw",
+        label: "Bmw"
+    },
+    {
+        id: 6,
+        value: "seat",
+        label: "Seat"
+    },
+    {
+        id: 7,
+        value: "alfa_romeo",
+        label: "Alfa Romeo"
+    },
+    {
+        id: 8,
+        value: "kia",
+        label: "Kia"
+    },
+    {
+        id: 9,
+        value: "hyundai",
+        label: "Hyundai"
+    },
+    {
+        id: 10,
+        value: "honda",
+        label: "Honda"
+    },
+    {
+        id: 11,
+        value: "toyota",
+        label: "Toyota"
+    },
+]
+
+export default vehicle_manufacturers
