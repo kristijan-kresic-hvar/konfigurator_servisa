36f5898638b6f3d845525ca7b9a0e70f65b9f1bc
kristijan-kresic-hvar <kristijan.kresic.dev@gmail.com>
2022-12-05 15:00:42 +0100

    Added confetti on form send

--- src/components/Configurator/steps/Success/Success.jsx
+++ src/components/Configurator/steps/Success/Success.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
 
+// third party libraries
+import Lottie from 'react-lottie'
+
+// local assets
+import confettiAnimation from '../../../../lotties/confetti.json'
+
 // local components
 import Button from '../../..//Button/Button'
 
+const defaultAnimationOptions = {
+    loop: false,
+    autoplay: true,
+    animationData: confettiAnimation,
+    rendererSettings: {
+        preserveAspectRatio: 'xMidYMid slice'
+    }
+};
+
 const Success = () => {
     return (
-        <div className="w-full h-full flex flex-col justify-center items-center text-center">
+        <div className="w-full h-full flex flex-col justify-center items-center text-center relative">
+            <div className="absolute top-0 left-0 right-0 bottom-0 pointer-events-none">
+                <Lottie
+                    isClickToPauseDisabled={true}
+                    options={defaultAnimationOptions}
+                    height={"100%"}
+                    width={"100%"}
+                />
+            </div>
             <h2 className="text-2xl mb-5">Vaša prijava je uspješno poslana</h2>
             <p className="max-w-[55ch] mb-16">
                 Vaša prijava je uspješno poslana i zaprimljena. Kontaktirati ćemo
