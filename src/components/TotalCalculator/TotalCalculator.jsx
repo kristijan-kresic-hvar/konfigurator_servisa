c4993e8acb5907c7fd0b8752ba3859274711ae10
kristijan-kresic-hvar <kristijan.kresic.dev@gmail.com>
2022-12-05 02:25:13 +0100

    Initial overview component setup

--- src/components/TotalCalculator/TotalCalculator.jsx
+++ src/components/TotalCalculator/TotalCalculator.jsx
@@ -1,33 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 // third party libraries
 import PropTypes from 'prop-types'
 
-const TotalCalculator = ({ items, discountAmount }) => {
+// local functions
+import useHelpers from '../../hooks/useHelpers'
 
-    const calculateTotal = () => {
+const sizeClasses = {
+    small: 'text-sm',
+    medium: 'text-base',
+    large: 'text-lg',
+}
+
+const TotalCalculator = ({ items, discountAmount, size, discountInfo }) => {
+
+    const { formatPrice } = useHelpers()
+
+    const [calculatedDiscount, setCalculatedDiscount] = useState(0)
+
+    const calculateTotal = (includeDiscount = true) => {
         let total = 0
         total = items.reduce((acc, item) => {
             return acc + item.price
         }, 0)
 
-        if (discountAmount) {
+        if (includeDiscount && discountAmount) {
             return total - (total * (discountAmount / 100))
         }
 
         return total
     }
 
+    useEffect(() => {
+        setCalculatedDiscount(calculateTotal(false) * (discountAmount / 100))
+    }, [discountAmount])
+
     return (
-        <div className="text-2xl uppercase">
-            Ukupno: <span className='font-bold'>{calculateTotal().toFixed(2).replace('.', ',')}</span>
+        <div className={`${sizeClasses[size] || 'text-2xl'}`}>
+            {discountInfo && discountAmount > 0 &&
+                <div>Popust ({discountAmount}%): -{formatPrice(calculatedDiscount)} KN</div>
+            }
+            <p className="uppercase text-right mt-1">Ukupno: <span className='font-bold'>{formatPrice(calculateTotal())} KN</span></p>
         </div>
     )
 }
 
 TotalCalculator.propTypes = {
     items: PropTypes.array.isRequired,
-    discountAmount: PropTypes.number
+    discountAmount: PropTypes.number,
+    size: PropTypes.oneOf(['small', 'medium', 'large']),
+    discountInfo: PropTypes.bool,
 }
 
 export default TotalCalculator
